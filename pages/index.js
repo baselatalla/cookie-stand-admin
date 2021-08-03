90a5a9ad3a67a196170f059d9b564f967c427d21
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-08-03 22:15:51 +0300

    finish lab-38

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,47 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useState } from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Form from '../components/createForm'
+import Table from '../components/reportTable'
+import {time} from '../data'
+
 export default function Home() {
 
   const [newBranchs, setnewBranchs] = useState([]);
+  
+  const [totals, setTotals] = useState([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  
+  function storInArray(max, min, avg){
+    let array = [];
+    array[15] = 0;
+    for (let i = 0 ; i < 15; i++ ){
+      let rand = Math.floor(Math.random() * (max - min + 1) + min);
+      let numOfCookies = Math.floor(avg * rand);
+      array[i] = numOfCookies;
+      array[15] += array[i];
+      totals[i]+= array[i]
+      
+      
+      
+    }
+    totals[15] += array[15]
+    return array;
+  }
 
   function newlocationHandler(event){
     event.preventDefault();
-
+    let arrayData = storInArray(parseInt(event.target.maxcust.value), parseInt(event.target.mincust.value), parseInt(event.target.avgcookie.value))
     const branch = {
       // id: newBranchs.length + 1,
       location: event.target.location.value,
       minCustomer: event.target.mincust.value,
       maxCustomer: event.target.maxcust.value,
       avgCookies: event.target.avgcookie.value,
+      randCookPerHour: arrayData
+      
+      
     }
     
     setnewBranchs([...newBranchs , branch]);
@@ -27,55 +55,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="flex bg-green-500 text-black-100 p-3 items-center">
-        <h1 className="font-semibold text-2xl font-sans">Cookie Stand Admin</h1>
-      </header>
+      <Header/>
 
       <main className=" flex flex-col my-10 justify-center mx-auto h-full items-center">
 
-        <div className=" bg-green-300 rounded-md w-3/4">
-          <legend className=" mb-4 text-center">
-            <h2 className=" m-10 font-medium text-2xl ">Create Cookie Stand  </h2>
+        <Form newlocationHandler= {newlocationHandler}/>
 
-            <form className="flex flex-wrap mx-3" onSubmit={newlocationHandler}>
-              <label className=" flex w-full my-5 "><p className=" w-1/12">Location</p>
-              <input className=" w-11/12 " name="location" />
-              </label>
-              <label className="w-3/12" ><p class="text-sm">Minimum Customers per Hour </p>
-              <input className="px-5 w-11/12 " name="mincust" type="number" min="0"/>
-              </label>
-              <label className="w-3/12 " ><p class="text-sm">Maximum Customers per Hour</p>
-              <input className=" px-5 w-11/12" name="maxcust" type="number" min="0"/>
-              </label>
-              <label className="w-3/12" ><p class="text-sm">Average Cookies per Sale </p>
-              <input className="px-5 w-11/12" name="avgcookie" type="number" step="0.1"></input>
-              </label>
-              <input className=" bg-green-500 w-3/12 text-center" type="submit" value="Create" />
-            </form>
-
-          </legend>
-        </div>
-
-        <div className="flex flex-col text-center">
-          <p class=" p-5 ">Report Table Coming Soon... </p>
-
-          {
-              newBranchs.map(item => {
-                return(
-                  
-                    <p class="font-mono text-gray-500">{JSON.stringify(item)}</p>
-                  
-                )
-              })
-            }
-
-        </div>
+        <Table time={time} newBranchs={newBranchs} totals={totals}/>
 
       </main>
 
-      <footer className="bg-green-500 text-black-100 p-4 ">
-        <p>&copy;2021</p>
-      </footer>
+      <Footer newBranchs={newBranchs}/>
     </div>
   )
 }
---
+++ pages/overview.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function OverView() {
+    return (
+        <div>
+            <Link href="/"><a>Return to Main page</a></Link>
+        </div>
+    )
+}
